a0863814e7718bcc445e04ac5798983674705727
pu2oli <38104346+pu2oli@users.noreply.github.com>
2022-06-06 17:26:41 -0300

    v1

--- virtualkey.php
+++ virtualkey.php
@@ -1,5 +1,10 @@
 <!DOCTYPE html>
 <html>
+<head>
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
 <body>
 <script>
 	function GetKey($btn) {
@@ -12,38 +17,50 @@
 </script>
 
 <?php
-
-$base = @$_REQUEST['base'];
-$key = @$_REQUEST['key'];
-
-echo 'Key: ' . $key . ' | base: ' . $base . ' | Numeros: ';
-$arrComb = array();
-for ($i = 0; $i <= 5; ++$i) {
-	$arrNumb = array();
-	if (substr($key, $i, 1) == 'A') {
-		$arrNumb[] = substr($base, 0, 1);
-		$arrNumb[] = substr($base, 1, 1);
-	} else if (substr($key, $i, 1) == 'B') {
-		$arrNumb[] = substr($base, 2, 1);
-		$arrNumb[] = substr($base, 3, 1);
-	} else if (substr($key, $i, 1) == 'C') {
-		$arrNumb[] = substr($base, 4, 1);
-		$arrNumb[] = substr($base, 5, 1);
-	} else if (substr($key, $i, 1) == 'D') {
-		$arrNumb[] = substr($base, 6, 1);
-		$arrNumb[] = substr($base, 7, 1);
-	} else if (substr($key, $i, 1) == 'E') {
-		$arrNumb[] = substr($base, 8, 1);
-		$arrNumb[] = substr($base, 9, 1);
+	// Gerador de GUID
+	function guidv4($data) {
+    assert(strlen($data) == 16);
+    $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
+    $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
+    return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
 	}
-	$arrComb[] = $arrNumb;
-}
 
-foreach ($arrComb as $nr) {
-	echo @$nr[0] . @$nr[1];
-}
-echo '<hr>';
+	// Recupera valores enviados pelo FrontEnd
+	$guid = @$_REQUEST['guid'];
+	$base = @$_REQUEST['base'];
+	$key = @$_REQUEST['key'];
 
+	echo 'GUID: Key: ' . $key . ' | base: ' . $base . ' | Numeros: ';
+
+	// Trata/Decodifica valores recebidos pelo FrontEnd
+	$arrComb = array();
+	for ($i = 0; $i <= 5; ++$i) {
+		$arrNumb = array();
+		if (substr($key, $i, 1) == 'A') {
+			$arrNumb[] = substr($base, 0, 1);
+			$arrNumb[] = substr($base, 1, 1);
+		} else if (substr($key, $i, 1) == 'B') {
+			$arrNumb[] = substr($base, 2, 1);
+			$arrNumb[] = substr($base, 3, 1);
+		} else if (substr($key, $i, 1) == 'C') {
+			$arrNumb[] = substr($base, 4, 1);
+			$arrNumb[] = substr($base, 5, 1);
+		} else if (substr($key, $i, 1) == 'D') {
+			$arrNumb[] = substr($base, 6, 1);
+			$arrNumb[] = substr($base, 7, 1);
+		} else if (substr($key, $i, 1) == 'E') {
+			$arrNumb[] = substr($base, 8, 1);
+			$arrNumb[] = substr($base, 9, 1);
+		}
+		$arrComb[] = $arrNumb;
+	}
+	// Debug - Exibe na tela 
+	foreach ($arrComb as $nr) {
+		echo @$nr[0] . @$nr[1];
+	}
+	echo '<hr>';
+
+	// Prepara matriz de combinaçoes
   $max = (1 << strlen($key));
   $arrAux = array();
   for ($i = 0; $i < $max; $i++) {
@@ -52,6 +69,7 @@ echo '<hr>';
 	
 	echo '<hr>';
 	
+	// Gera combinaçoes de senhas para a cadeia recebida (2^6 para 6 posicoes)
 	$combs = array();
   foreach ($arrAux as $vals) {
   	$nr = '';
@@ -61,7 +79,7 @@ echo '<hr>';
   	$combs[] = $nr;
   }
 
-    //verificando as combinações possíveis
+    // Realiza a validacao do hash recebido 
 		$lista = '';
 		$auth = false;
     foreach ($combs as $value) {
@@ -74,9 +92,9 @@ echo '<hr>';
 		}
 	
 		if ($auth) {
-			echo "Autenticado!";
+			echo '<h3 id="login-sucess" style="color:green" >Login realizado com sucesso!</h3>';
 		} else if ($lista != '') {
-			echo "Usuario ou senha inválido!";
+			echo '<h3 id="login-error" style="color:red">Usuário e senha inválidos!</h3>';
 		}
 
 // Geração da semente
@@ -99,55 +117,29 @@ $base = '';
 for ($i = 0; $i < count($a); ++$i) {
 	$base .= $a[$i];
 }
-
+echo '<h3 class="text-center text-info">Login</h3>';
 echo '<form id="frm" name="frm" action="virtualkey.php" method="post">';
-echo '<input type="text" name="key" id="key" value="">';
-echo '<input type="text" name="base" id="base" value="' . $base . '">';
+echo '<input type="text" class="form-control" name="guid" id="guid" value="' . guidv4(openssl_random_pseudo_bytes(16)) . '">';
+echo '<input type="text" class="form-control" name="key" id="key" value="">';
+echo '<input type="text" class="form-control" name="base" id="base" value="' . $base . '">';
 echo '<table>';
 echo '	<tr>';
-echo '		<td><input type="button" id="A" onclick="GetKey(this)" value="[' . $a[0] . '/'. $a[1] . ']"/></td>';
-echo '		<td><input type="button" id="B" onclick="GetKey(this)" value="[' . $a[2] . '/'. $a[3] . ']"/></td>';
-echo '		<td><input type="button" id="C" onclick="GetKey(this)" value="[' . $a[4] . '/'. $a[5] . ']"/></td>';
+echo '		<td><input type="button" class="form-control" id="A" onclick="GetKey(this)" value="[' . $a[0] . '/'. $a[1] . ']"/></td>';
+echo '		<td><input type="button" class="form-control" id="B" onclick="GetKey(this)" value="[' . $a[2] . '/'. $a[3] . ']"/></td>';
+echo '		<td><input type="button" class="form-control" id="C" onclick="GetKey(this)" value="[' . $a[4] . '/'. $a[5] . ']"/></td>';
 echo '	</tr>';
 echo '	<tr>';
-echo '		<td><input type="button" id="D" onclick="GetKey(this)" value="[' . $a[6] . '/'. $a[7] . ']"/></td>';
-echo '		<td><input type="button" id="E" onclick="GetKey(this)" value="[' . $a[8] . '/'. $a[9] . ']"/></td>';
-echo '		<td><input type="button" id="F" onclick="GetKey(this)" value="[<<]"/></td>';
+echo '		<td><input type="button" class="form-control" id="D" onclick="GetKey(this)" value="[' . $a[6] . '/'. $a[7] . ']"/></td>';
+echo '		<td><input type="button" class="form-control" id="E" onclick="GetKey(this)" value="[' . $a[8] . '/'. $a[9] . ']"/></td>';
+echo '		<td><input type="button" class="form-control" id="F" onclick="GetKey(this)" value="[<<]"/></td>';
 echo '	</tr>';
 echo '</table>';
 echo '<input type="submit" value="Enviar" />';
 echo '</form>';
 
-// var_dump($a);
-#for($i = 0; count($a); ++$i) {
-#	echo $a[$i] . '-';
-#}
-
-
-
 echo '<br>';
 
-
-echo $lista;
-// If you want to fetch multiple values you can try this:
-// print_r( array_intersect_key( $a, array_flip( array_rand( $a, $n ) ) ) );
-// echo '<br>';
-// // If you want to re-index keys wrap the call in 'array_values':
-// print_r( array_values( array_intersect_key( $a, array_flip( array_rand( $a, $n ) ) ) ) );
-
-function guidv4($data)
-{
-    assert(strlen($data) == 16);
-
-    $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
-    $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
-
-    return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
-}
-
-echo "GID: " . guidv4(openssl_random_pseudo_bytes(16));
-
-
+echo 'Combinações:<hr>' . $lista . '<hr>';
 
 ?>
 
